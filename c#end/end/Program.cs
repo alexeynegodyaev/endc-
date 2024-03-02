f1fc6063c5322481b0f1e23473e2e1a8d5a31dd7
alexeynegodyaev <160755251+alexeynegodyaev@users.noreply.github.com>
2024-03-02 17:25:59 +0300

    Add files via upload

---
+++ c#end/end/Program.cs
@@ -0,0 +1,30 @@
+﻿void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+string[] FilterArray(string[] array)
+{
+    int arrayLength = array.Length;
+    int n = 0;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        if (array[i].Length <= 3)
+         {
+            n += 1;
+        }
+    string[] resultArray = new string[n];
+    int i2 = 0;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultArray[i2] = array[i];
+            i2 += 1;
+        }
+    }
+
+    return resultArray;
+}
+string[] newArray = { "Hello", "world" };
+PrintArray(newArray);
+PrintArray(FilterArray(newArray));
